bcd2f51c6a8d7f805705e9541992fc6e534f304c
LeonardoCesca <leonardocescaf@gmail.com>
2019-07-21 18:29:13 -0300

    ADD core module

---
+++ src/app/modules/core/core.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderContainerComponent } from './containers/header-container/header-container.component';
+
+
+
+@NgModule({
+  declarations: [HeaderContainerComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CoreModule { }
