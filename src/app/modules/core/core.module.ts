47ab5d362dfc97dc52cfc82521a204b68e48db7b
LeonardoCesca <leonardocescaf@gmail.com>
2019-07-21 18:37:34 -0300

    UPDATE core module

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -3,11 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HeaderContainerComponent } from './containers/header-container/header-container.component';
 
 
-
 @NgModule({
   declarations: [HeaderContainerComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [HeaderContainerComponent]
 })
 export class CoreModule { }
