91d9f2ba7bb59442957629753af8dff80fc07bed
LeonardoCesca <leonardocescaf@gmail.com>
2019-07-21 20:13:24 -0300

    ADD routes into app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
 
-
+const routes: Routes = [
+  {
+    path: 'homes',
+    loadChildren: './modules/homes/homes.module#HomesModule'
+  },
+  {
+    path: '',
+    redirectTo: 'homes',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'homes'
+  }
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
